66d8002eebc8d3e1a27d172894782ceba0980498
Aaditya Kumar Rai <36476997+aaditya1111@users.noreply.github.com>
2019-11-21 23:55:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.app import App
+
+__author__ = "Akas Antony"
+__version__ = "1.0.0"
+__maintainer__ = "Akas Antony"
+__email__ = "antony.akas@gmail.com"
+
+def main():
+    app = App()
+    app.run()
+
+if __name__ == "__main__":
+	main()
